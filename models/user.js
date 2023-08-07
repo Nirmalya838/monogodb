b96fe168e69a381e11a6b5c2bc6ed7b1886a1807
Nirmalya838 <122223948+Nirmalya838@users.noreply.github.com>
2023-08-07 18:47:53 +0530

    Orders page are done

--- models/user.js
+++ models/user.js
@@ -76,7 +76,37 @@ class User {
       {$set:{cart:{items:updatedCartItems}}}
       )
   }
+  addOrder(){
+    const db= getDb();
+    return this.getCart().then(products=>{
+      const order={
+        items:products,
+        users:{
+          _id:new ObjectId(this._id),
+          name:this.name,
+          email:this.email
+        }
+      }
+      return db.collection('orders').insertOne(order)
+    })
+    .then(result=>{
+      this.cart={items:[]};
+      return db
+      .collection('users')
+      .updateOne(
+        {_id:new ObjectId(this._id)},
+        {$set:{cart:{items:[]}}}
+        )
+    })
+    
+  }
 
+  getOrder(){
+    const db=getDb();
+    return db.collection('orders')
+    .find({'users._id': new ObjectId(this._id)})
+    .toArray()
+  }
   static findById(userId) {
     const db = getDb();
     return db.collection('users')
